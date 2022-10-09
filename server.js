0e9ff5f9752724df30c89980cc94252d9a2969fc
PabloMesquida <pablomesquida67@gmail.com>
2022-10-09 02:46:18 +0200

    corss

--- server.js
+++ server.js
@@ -3,10 +3,11 @@ const jsonServer = require("json-server");
 const server = jsonServer.create();
 const router = jsonServer.router("db.json");
 const middlewares = jsonServer.defaults();
-
+const cors = require("cors");
 server.use(middlewares);
 // Add this before server.use(router)
 
+server.use(cors());
 server.use(router);
 server.listen(3000, () => {
   console.log("JSON Server is running");
